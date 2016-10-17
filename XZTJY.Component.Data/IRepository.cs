5839b4be0c30d7bed5b4dcc93ac5e1065e6d2805
zhoule2016 <13488528900@163.com>
2016-10-17 23:33:20 +0800

    与业务实体无关的数据组件 GMF.Component.Data 已经搭建完成

---
+++ XZTJY.Component.Data/IRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using XZTJY.Component.Tools;
+
+namespace XZTJY.Component.Data
+{
+    /// <summary>
+    ///     定义仓储模型中的数据标准操作
+    /// </summary>
+    /// <typeparam name="TEntity">动态实体类型</typeparam>
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        #region 属性
+
+        /// <summary>
+        ///     获取 当前实体的查询数据集
+        /// </summary>
+        IQueryable<TEntity> Entities { get; }
+
+        #endregion
+
+        #region 公共方法
+
+        /// <summary>
+        ///     插入实体记录,每个操作方法都带有一个 isSave 可选参数，是为了单个实体操作的需要，免去了每次都要调用 context.SaveChanged()的麻烦。如果是进行多个实体的单元事务操作，就需要把这个参数设置为 false 。
+        /// </summary>
+        /// <param name="entity"> 实体对象 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Insert(TEntity entity, bool isSave = true);
+
+        /// <summary>
+        ///     批量插入实体记录集合
+        /// </summary>
+        /// <param name="entities"> 实体记录集合 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Insert(IEnumerable<TEntity> entities, bool isSave = true);
+
+        /// <summary>
+        ///     删除指定编号的记录
+        /// </summary>
+        /// <param name="id"> 实体记录编号 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Delete(object id, bool isSave = true);
+
+        /// <summary>
+        ///     删除实体记录
+        /// </summary>
+        /// <param name="entity"> 实体对象 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Delete(TEntity entity, bool isSave = true);
+
+        /// <summary>
+        ///     删除实体记录集合
+        /// </summary>
+        /// <param name="entities"> 实体记录集合 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Delete(IEnumerable<TEntity> entities, bool isSave = true);
+
+        /// <summary>
+        ///     删除所有符合特定表达式的数据
+        /// </summary>
+        /// <param name="predicate"> 查询条件谓语表达式 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Delete(Expression<Func<TEntity, bool>> predicate, bool isSave = true);
+
+        /// <summary>
+        ///     更新实体记录
+        /// </summary>
+        /// <param name="entity"> 实体对象 </param>
+        /// <param name="isSave"> 是否执行保存 </param>
+        /// <returns> 操作影响的行数 </returns>
+        int Update(TEntity entity, bool isSave = true);
+
+        /// <summary>
+        ///     查找指定主键的实体记录
+        /// </summary>
+        /// <param name="key"> 指定主键 </param>
+        /// <returns> 符合编号的记录，不存在返回null </returns>
+        TEntity GetByKey(object key);
+
+        #endregion
+    }
+}
